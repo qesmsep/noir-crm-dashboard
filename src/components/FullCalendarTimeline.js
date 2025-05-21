93a7ea0d1c807cb7392ea0b8f466feb665972048
Tim Wirick <qesmsep@Tims-MacBook-Air.local>
2025-05-21 16:09:35 -0500

    Fix FullCalendarTimeline CSS imports

--- src/components/FullCalendarTimeline.js
+++ src/components/FullCalendarTimeline.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import FullCalendar from '@fullcalendar/react';
 import resourceTimelinePlugin from '@fullcalendar/resource-timeline';
-import '@fullcalendar/common/main.css';
+import '@fullcalendar/core/main.css';
+import '@fullcalendar/resource-timeline/main.css';
 
 export default function FullCalendarTimeline({ reloadKey }) {
   const [resources, setResources] = useState([]);
