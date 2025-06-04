ba081cdd2f4fc4ee9f95499f379bd61eaebd974b
Tim Wirick <qesmsep@Mac.attlocal.net>
2025-06-04 16:03:44 -0500

    Switch to node-fetch for OpenPhone API call in sendText.js

--- api/sendText.js
+++ api/sendText.js
@@ -1,4 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
+import fetch from 'node-fetch';
 
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY);
 
