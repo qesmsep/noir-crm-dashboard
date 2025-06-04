e6fe49ee111f79655d6ab8915c1d193ae3b8ac74
Tim Wirick <qesmsep@Mac.attlocal.net>
2025-06-04 15:56:01 -0500

    Fix OpenPhone API Authorization header: remove Bearer, use API key directly

--- api/sendText.js
+++ api/sendText.js
@@ -39,7 +39,7 @@ export default async function handler(req, res) {
       const response = await fetch('https://api.openphone.co/v1/messages', {
         method: 'POST',
         headers: {
-          'Authorization': `Bearer ${OPENPHONE_API_KEY}`,
+          'Authorization': OPENPHONE_API_KEY,
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
