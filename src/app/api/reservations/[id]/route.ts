70ef7a6f2ab647d554c1e0f2bdea642ddc855504
Tim Wirick <qesmsep@Tims-MacBook-Air.local>
2025-06-28 12:08:47 -0500

    reservation modal fix

--- src/app/api/reservations/[id]/route.ts
+++ src/app/api/reservations/[id]/route.ts
@@ -1,4 +1,4 @@
-import { NextResponse } from 'next/server';
+import { NextResponse, NextRequest } from 'next/server';
 import { createClient } from '@supabase/supabase-js';
 
 console.log('Supabase URL:', process.env.NEXT_PUBLIC_SUPABASE_URL);
@@ -10,7 +10,7 @@ const supabase = createClient(
 );
 
 export async function PATCH(
-  request: Request,
+  request: NextRequest,
   { params }: { params: { id: string } }
 ) {
   const { id } = params;
@@ -70,7 +70,7 @@ export async function PATCH(
 }
 
 export async function GET(
-  request: Request,
+  request: NextRequest,
   { params }: { params: { id: string } }
 ) {
   const { id } = params;
@@ -115,7 +115,7 @@ export async function GET(
 }
 
 export async function DELETE(
-  request: Request,
+  request: NextRequest,
   { params }: { params: { id: string } }
 ) {
   try {
