11e5c7f9d1a601266dfb34efd00e33fbc9e65aa0
Tim Wirick <qesmsep@Tims-MacBook-Air.local>
2025-06-30 18:20:10 -0500

    Exclude checkpoint directories from build to fix TypeScript errors - Added /checkpoints/ and /Ω_historic/ to .gitignore - Removed checkpoint directories from git tracking - Prevents TypeScript errors from backup files during build

--- checkpoints/pre_styling_20250621_010743/components/ChakraClientProvider.js
+++
@@ -1,7 +0,0 @@
-'use client';
-import { ChakraProvider } from '@chakra-ui/react';
-import { theme } from '../theme';
-
-export default function ChakraClientProvider({ children }) {
-  return <ChakraProvider theme={theme}>{children}</ChakraProvider>;
-} 
